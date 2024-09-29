57db31bf4450869d568148c8a51d8428fcb6eb7b
Aestrada50 <agestra217@gmail.com>
2024-09-29 03:46:00 -0600

    look

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blue,
-      )
+        appBar: AppBar(
+          title: Text("Med Friend"),
+          backgroundColor: Colors.deepPurple[100],
+          leading: Icon(Icons.menu),
+
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[100],
+              //curving the corners a bit
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Icon(
+              Icons.favorite,
+              color: Colors.blue[50],
+              size: 150,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
